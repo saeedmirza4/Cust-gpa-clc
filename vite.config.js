6e4bb053397fad771431ae2772ffc9fecb4d9a93
saeedmirza49 <sssf26mirza@gmail.com>
2025-07-05 13:11:11 +0500

    next

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
+    chunkSizeWarningLimit: 1600
+  }
 })
